d087afdf2c2367a4c6876342075d0d73c21c974a
Joshua Seitz <jseitz2@luc.edu>
2020-10-09 16:54:16 -0500

    first commit

---
+++ CoreDataTest/PersistenceController.swift
@@ -0,0 +1,25 @@
+//
+//  PersistenceController.swift
+//  CoreDataTest
+//
+//  Created by Josh on 10/9/20.
+//  Copyright © 2020 Joshua Seitz. All rights reserved.
+//
+
+import Foundation
+
+struct PersistenceController {
+	
+	func save() {
+		
+	}
+	
+	func get() {
+		
+	}
+	
+	func delete() {
+		
+	}
+	
+}
---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Josh on 10/9/20.
+//  Copyright © 2020 Joshua Seitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
